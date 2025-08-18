336a107b7f2875ac7e41eaf2eebaae6a037a66fa
maruf-hussain <engmarufhussain@gmail.com>
2025-08-18 14:07:36 +0600

    add event

---
+++ js/event.js
@@ -0,0 +1,7 @@
+function makeRed(){
+   document.getElementById('brand').style.color = 'red';
+}
+
+document.getElementById('green').addEventListener('click', function(){
+  document.getElementById('fruits').style.color = 'green';
+}); 
---
+++ js/main.js
@@ -0,0 +1,16 @@
+// const sections = document.querySelectorAll('section');
+//     for(const section of sections){
+//     section.style.border = '1px solid red'
+//     section.style.color = 'yellow'
+//     section.style.backgroundColor = 'gray'
+//     section.style.marginTop = '20px'
+//     section.style.padding = '10px'
+//     section.style.borderRadius = '20px'
+// };
+
+// const brand = document.getElementById('brand');
+// brand.style.backgroundColor = 'darkblue'
+
+
+
+
