1e13eb5f1210d14853f7d87a5603884ba4847e23
maruf-hussain <engmarufhussain@gmail.com>
2025-08-18 14:28:50 +0600

    input change text

--- js/event.js
+++ js/event.js
@@ -5,3 +5,13 @@ function makeRed(){
 document.getElementById('green').addEventListener('click', function(){
   document.getElementById('fruits').style.color = 'green';
 }); 
+
+document.getElementById('text-button').addEventListener('click',function(){
+    const inputText = document.getElementById('text-input').value;
+    const textDisplay = document.getElementById('text-display');
+    textDisplay.innerText = inputText;
+    textDisplay.style.color = 'red';
+    textDisplay.style.fontSize = '30px';
+    inputText.value = '';
+    
+})
